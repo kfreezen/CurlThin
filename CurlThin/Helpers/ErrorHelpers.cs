ec0e5a32ff902dbda74173c9c0d44b0994843052
Kent Friesen <kfreezen@gmail.com>
2018-07-25 09:38:07 -0400

    Added a RequestBodyCopier for copying POST request bodies from a byte array.

---
+++ CurlThin/Helpers/ErrorHelpers.cs
@@ -0,0 +1,36 @@
+﻿using CurlThin.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurlThin.Helpers
+{
+    /// <summary>
+    /// A little helper class to simplify certain types of error checking on CURL codes.
+    /// </summary>
+    public static class ErrorHelpers
+    {
+        /// <summary>
+        /// Returns whether the given CURLcode is an SSL error code.
+        /// </summary>
+        /// <param name="curlCode"></param>
+        /// <returns></returns>
+        public static bool IsSslError(CURLcode curlCode)
+        {
+            switch(curlCode)
+            {
+                case CURLcode.PEER_FAILED_VERIFICATION:
+                case CURLcode.SSL_CIPHER:
+                case CURLcode.SSL_ENGINE_INITFAILED:
+                case CURLcode.SSL_CONNECT_ERROR:
+                case CURLcode.SSL_INVALIDCERTSTATUS:
+                case CURLcode.SSL_ISSUER_ERROR:
+                case CURLcode.SSL_SHUTDOWN_FAILED:
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ CurlThin/Helpers/RequestBodyCopier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CurlThin.Helpers
+{
+    /// <summary>
+    /// Use this helper class for CURLoption.READFUNCTION
+    /// 
+    ///
+    /// </summary>
+    public class RequestBodyCopier : IDisposable
+    {
+        public RequestBodyCopier(Stream requestBody, long contentLength)
+        {
+            ContentLength = contentLength;
+
+            DataHandler = (data, size, nmemb, userdata) =>
+            {
+                int length = (int)size * (int)nmemb;
+                var buffer = new byte[length];
+                RequestBodyStream.Read(buffer, 0, length);
+                Marshal.Copy(buffer, 0, data, length);
+                return (UIntPtr)length;
+            };
+        }
+
+        public Stream RequestBodyStream { get; private set; }
+        public long ContentLength { get; private set; }
+
+        public CurlNative.Easy.DataHandler DataHandler { get; private set; }
+
+        public void Dispose()
+        {
+            // Not much to dispose here. We don't want to dispose the stream because the caller might not be done with it.
+        }
+    }
+}
